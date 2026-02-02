6a90922a43ef00e6a2c91021158a3ae91cc5b4d4
subhansalman <subhansalman492@gmail.com>
2026-02-02 06:52:40 +0500

    first assignment completed

---
+++ app.js
@@ -0,0 +1,24 @@
+
+    console.log("Before Changing anything", newVar )
+
+
+{
+    var newVar="A/B"
+    console.log("Inside Block but value change", newVar)
+}
+
+console.log("Afetr Block value", newVar)
+
+console.log("xxxxxxxxxxx")
+
+
+
+console.log("Before Changing any value", newVar)
+{
+    let newVar="newvalue"
+    console.log("Inside Block",newVar)
+}
+
+console.log("After Block",newVar)
+
+console.log("xxxxxxxxxxx")
