1e385f2ce79ceb7f882cf132c2f227e220cdbb22
subhansalman <subhansalman492@gmail.com>
2026-02-02 19:32:14 +0500

    section 1 part 1.2 completed

--- app.js
+++ app.js
@@ -22,3 +22,27 @@ console.log("Before Changing any value", newVar)
 console.log("After Block",newVar)
 
 console.log("xxxxxxxxxxx")
+
+
+
+var tasktwo='4+a+a4'
+
+try {
+    console.log("var ki initial value ", tasktwo)
+    var tasktwo='a+3ab+b'
+    console.log("var re declare hogaya successfully ", tasktwo)
+} catch (error) {
+    console.log(error.message)
+}
+
+
+let tasktwoforlet='2+3/7*6'
+
+try {
+    console.log("Let ki initial value ", tasktwoforlet)
+    let tasktwoforlet='6+8/9*5'
+    console.log("let ki redeclare karna ka baad value ", tasktwoforlet)
+} catch (error) {
+    console.log(error.message)
+}
+
